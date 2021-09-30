f04d6974aed2def1b6514b7f74b183af32e35832
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-09-30 06:44:23 -0300

    list of categories

---
+++ src/carrefour.ts
@@ -0,0 +1,20 @@
+
+import Category from "./types/Category";
+
+
+
+const carrefour: Category[] = [
+    { id: 1, name: 'Bebidas Alcoólicas', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/bebidas-alcoolicas#crfimt=home|mercado|bns|botton|bebidas-alcoolicas_020821|2"},
+    { id: 2, name: 'Bebidas', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/bebidas#crfimt=home|mercado|bns|botton|bebidas_020821|3"},
+    { id: 3, name: 'Açougue', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/acougue-e-peixaria#crfimt=home|mercado|bns|botton|acougue_020821|4"},
+    { id: 4, name: 'Higiene e Beleza', url: "https://mercado.carrefour.com.br/higiene-e-beleza/#crfimt=home|mercado|bns|botton|higiene-e-beleza_020821|5"},
+    { id: 5, name: 'Limpeza', url: "https://mercado.carrefour.com.br/limpeza-e-lavanderia/#crfimt=home|mercado|bns|botton|limpeza_020821|6"},
+    { id: 6, name: 'Doces, Biscoitos e Confeitaria', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/doces-biscoitos-e-confeitaria#crfimt=home|mercado|bns|botton|doces-biscoitos-e-confeitaria_020821|7"},
+    { id: 7, name: 'Pratos Prontos e Congelados', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/pratos-prontos-e-congelados#crfimt=home|mercado|bns|botton|congelados-e-pratos-prontos_020821|8"},
+    { id: 8, name: 'Frios e Laticínios', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/frios-iogurtes-e-laticinios#crfimt=home|mercado|bns|botton|frios-e-laticinios_020821|9"},
+    { id: 9, name: 'Utilidade de Casa', url: "https://mercado.carrefour.com.br/utilidades-e-casa/#crfimt=home|mercado|bns|botton|utilidades-de-casa_020821|10"},
+    { id: 10, name: 'Leites', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/leites-formulas-e-fortificados#crfimt=home|mercado|bns|botton|leites_020821|11"},
+    { id: 11, name: 'Mercenaria', url: "https://mercado.carrefour.com.br/alimentos-e-bebidas/mercearia/alimentos-basicos#crfimt=home|mercado|bns|botton|mercearia_020821|12"}
+];
+
+export default carrefour;
